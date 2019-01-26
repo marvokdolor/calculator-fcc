4e2859d530f33021af912104edbcab3deaecf194
marvokdolor <marvourneen@mkdolor.com>
2019-01-26 15:52:43 -0500

    Initial commit of all files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,102 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+References:
+- https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+- https://codepen.io/marvokdolor/pen/Vmoege?editors=0010
+- https://www.w3schools.com/jsref/jsref_eval.asp
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+*/
+import React from "react"
+import "./style.css"
+import calcData from "./calcData.js"
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            displayText: "0",
+            num: "",
+            arrInput: []
+        }
+
+    }
+    updateInput = (input) => {
+
+        switch (input){
+            case("equals"):
+                this.setState(prevState => ({
+                    num: eval(prevState.num)
+                }))
+                break
+            case("clear"):
+                this.setState({
+                    num: ""
+                })
+                break
+            default:
+                this.setState(prevState => ({
+                    num: prevState.num + input
+                }))
+                break
+        }
+    }
+    /* TODOs
+    1. When inputting numbers, my calculator should not allow a number to begin with multiple zeros.
+    2. Two . in one number should not be accepted.
+    3.  If 2 or more operators are entered consecutively, the operation performed should be the last operator entered.
+    */
+
+    render() {
+        const displayText = this.state.num ? this.state.num : "0"
+        return (
+            <div id="calculator">
+                <h1>Calculator!</h1>
+                <Display displayText={displayText}
+                />
+                {calcData.map( calcbutton =>
+                    <CalcButton
+                        key={calcbutton.id}
+                        calcbutton={calcbutton}
+                        updateInput={this.updateInput}
+                    />
+                )}
+            </div>
+        )
+    }
 }
 
-export default App;
+class CalcButton extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick = (e) => {
+        // console.log(e.target.value)
+        this.props.updateInput(e.target.value)
+    }
+
+    render() {
+        return (
+            <button
+                id={this.props.calcbutton.id}
+                className="button calc-button"
+                value={this.props.calcbutton.value}
+                onClick={this.handleClick}
+                >
+                {this.props.calcbutton.buttonText}
+            </button>
+        )
+    }
+}
+
+class Display extends React.Component {
+    render() {
+        return (
+            <div id="display" className="well">
+                {this.props.displayText}
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/calcData.js
@@ -0,0 +1,106 @@
+const calcData = [
+    {
+        id: "one",
+        buttonText : "1",
+        value: 1,
+        unicode: 49
+    },
+    {
+        id: "two",
+        buttonText : "2",
+        value: 2,
+        unicode: 50
+    },
+    {
+        id: "three",
+        buttonText : "3",
+        value: 3,
+        unicode: 51
+    },
+    {
+        id: "four",
+        buttonText : "4",
+        value: 4,
+        unicode: 52
+    },
+    {
+        id: "five",
+        buttonText : "5",
+        value: 5,
+        unicode: 53
+    },
+    {
+        id: "six",
+        buttonText : "6",
+        value: 6,
+        unicode: 54
+    },
+    {
+        id: "seven",
+        buttonText : "7",
+        value: 7,
+        unicode: 55
+    },
+    {
+        id: "eight",
+        buttonText : "8",
+        value: 8,
+        unicode: 56
+    },
+    {
+        id: "nine",
+        buttonText : "9",
+        value: 9,
+        unicode: 57
+    },
+    {
+        id: "zero",
+        buttonText : "0",
+        value: 0,
+        unicode: 48
+    },
+    {
+        id: "decimal",
+        buttonText : ".",
+        value: ".",
+        unicode: 190
+    },
+    {
+        id: "clear",
+        buttonText : "C",
+        value: "clear",
+        unicode: 67
+    },
+    {
+        id: "equals",
+        buttonText : "=",
+        value: "equals",
+        unicode: 187 /61
+    },
+    {
+        id: "add",
+        buttonText : "+",
+        value: "+",
+        unicode: 171 //187
+    },
+    {
+        id: "subtract",
+        buttonText : "-",
+        value: "-",
+        unicode: 189
+    },
+    {
+        id: "multiply",
+        buttonText : "x",
+        value: "*",
+        unicode: 170
+    },
+    {
+        id: "divide",
+        buttonText : "/",
+        value: "/",
+        unicode: 191
+    }
+]
+
+export default calcData
