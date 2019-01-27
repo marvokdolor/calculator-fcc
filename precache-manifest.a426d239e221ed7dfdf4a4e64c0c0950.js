bc6a14e64c4763c10bbd20013e96abbf9554461f
marvokdolor <marvourneen@mkdolor.com>
2019-01-27 16:17:07 -0500

    Updates

---
+++ precache-manifest.a426d239e221ed7dfdf4a4e64c0c0950.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99510751d8591a3cb7bb",
+    "url": "/calculator-fcc/static/js/runtime~main.99510751.js"
+  },
+  {
+    "revision": "9696563ad15447d5134b",
+    "url": "/calculator-fcc/static/js/main.9696563a.chunk.js"
+  },
+  {
+    "revision": "1801be334c1488c8c6da",
+    "url": "/calculator-fcc/static/js/1.1801be33.chunk.js"
+  },
+  {
+    "revision": "9696563ad15447d5134b",
+    "url": "/calculator-fcc/static/css/main.7eaf5970.chunk.css"
+  },
+  {
+    "revision": "df82bcf838d221fccd74ed820eca3dde",
+    "url": "/calculator-fcc/index.html"
+  }
+];
